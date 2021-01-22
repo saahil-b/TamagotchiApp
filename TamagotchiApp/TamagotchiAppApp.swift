5baea8d56287f96e79a77b404f248123dbb4841f
Saahil <bahadur.s@etoncollege.org.uk>
2021-01-22 11:59:50 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Rakesh Bahadur on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
